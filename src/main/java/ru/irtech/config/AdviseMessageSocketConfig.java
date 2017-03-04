07d5d37dce30bd3ee8416fb5a16044b7c6464778
Igor Bobko <limit-speed@yandex.ru>
2017-03-04 18:12:57 +0300

    Fixed cross domain connection

--- src/main/java/ru/irtech/config/AdviseMessageSocketConfig.java
+++ src/main/java/ru/irtech/config/AdviseMessageSocketConfig.java
@@ -13,9 +13,8 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class AdviseMessageSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/advises").withSockJS();
+        registry.addEndpoint("/advises").setAllowedOrigins("*").withSockJS();
     }
-
 
 }
 
