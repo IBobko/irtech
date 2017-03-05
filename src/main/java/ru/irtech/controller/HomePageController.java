f522c4cb04a7bc252686c320830045fe55066377
Igor Bobko <limit-speed@yandex.ru>
2017-03-05 03:45:23 +0300

    Changed port to 9000

--- src/main/java/ru/irtech/controller/HomePageController.java
+++ src/main/java/ru/irtech/controller/HomePageController.java
@@ -13,6 +13,6 @@ public class HomePageController {
     @RequestMapping("")
     @ResponseBody
     public String index() {
-        return "IrTech main page";
+        return "IrTech main page.";
     }
 }
