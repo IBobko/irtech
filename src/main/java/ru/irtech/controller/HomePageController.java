b33d93c7c016f9f9f84453834a2c5917b4ac0c36
Igor Bobko <limit-speed@yandex.ru>
2017-03-20 17:59:27 +0300

    Some changes of template engines

--- src/main/java/ru/irtech/controller/HomePageController.java
+++ src/main/java/ru/irtech/controller/HomePageController.java
@@ -2,7 +2,6 @@ package ru.irtech.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Igor Bobko <limit-speed@yandex.ru>.
@@ -16,8 +15,7 @@ public class HomePageController {
      * @return Text which provided users.
      */
     @RequestMapping("")
-    @ResponseBody
     public String index() {
-        return "<!doctype html>\n<html>\n<head>\n<script src=\"https://innsgo.ru/resources/js/adviser.js\"></script>\n<meta charset=\"utf-8\"/>\n</head>\n<body>\n</body>\n</html>";
+        return "index";
     }
 }
