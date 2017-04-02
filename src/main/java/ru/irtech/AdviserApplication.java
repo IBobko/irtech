99e36425465572505f1cf88cbc5a05c6c483a391
Igor Bobko <limit-speed@yandex.ru>
2017-04-02 20:34:42 +0300

    checkstyle fixes

--- src/main/java/ru/irtech/AdviserApplication.java
+++ src/main/java/ru/irtech/AdviserApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import ru.irtech.filter.JavascriptFilter;
 import ru.irtech.service.HostNameStatService;
 
