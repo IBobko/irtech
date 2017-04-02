81a2c8f52d52f3132e879735fa0a7917e9a75a08
iggytoto <iggytoto@gmail.com>
2017-04-02 16:55:49 +0300

    once more

--- src/main/java/ru/irtech/WebConfig.java
+++ src/main/java/ru/irtech/WebConfig.java
@@ -1,8 +1,10 @@
 package ru.irtech;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -15,10 +17,15 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     /**
      * enabling CORS for application
      *
-     * @param registry
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
     }
 }
+
