950e953c40aee3fd93250052a5de1f53f92e8dde
iggytoto <iggytoto@gmail.com>
2017-04-15 16:13:52 +0300

    Pearsons correlation processor implemented

---
+++ src/main/java/ru/irtech/analysis/Correlation/PearsonsCorrelationProcessor.java
@@ -0,0 +1,45 @@
+package ru.irtech.analysis.Correlation;
+
+import weka.attributeSelection.CorrelationAttributeEval;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+/**
+ * Created by Iggytoto on 15.04.2017.
+ * Correlation processor that uses Pearsons corellation method for processing
+ */
+public class PearsonsCorrelationProcessor implements ICorrelationProcessor {
+    /**
+     * Process the data
+     *
+     * @param csvFileName       input path to .csv file
+     * @param classColumnIndex  index of column that points on the class
+     * @param targetColumnIndex indexes of column that points on the target attribute
+     * @return
+     */
+    @Override
+    public Dictionary<Integer, Double> ParseCorrelation(String csvFileName, Integer classColumnIndex
+            , Integer[] targetColumnIndex) {
+        Dictionary<Integer, Double> result = new Hashtable<>();
+        try {
+            ConverterUtils.DataSource source = new ConverterUtils.DataSource(csvFileName);
+            Instances data = source.getDataSet();
+
+            data.setClassIndex(classColumnIndex);
+
+            CorrelationAttributeEval ceval = new CorrelationAttributeEval();
+            ceval.buildEvaluator(data);
+
+            for (Integer i : targetColumnIndex) {
+                result.put(i, ceval.evaluateAttribute(i));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage()); //TODO Implement Logger
+        } finally {
+            return result;
+        }
+    }
+}
---
+++ src/main/java/ru/irtech/analysis/OutliersAndExtremes/IOutlierProcessor.java
@@ -0,0 +1,8 @@
+package ru.irtech.analysis.OutliersAndExtremes;
+
+/**
+ * Created by Iggytoto on 15.04.2017.
+ */
+public interface IOutlierProcessor {
+    
+}
