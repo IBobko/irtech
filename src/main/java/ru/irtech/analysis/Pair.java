447b084c25e047e4c58887e79145798b4f757c46
Igor Bobko <limit-speed@yandex.ru>
2017-06-07 00:56:54 +0300

    importer works.

--- src/main/java/ru/irtech/analysis/Pair.java
+++ src/main/java/ru/irtech/analysis/Pair.java
@@ -1,81 +1,61 @@
 package ru.irtech.analysis;
 
-/*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
-
-import java.io.Serializable;
 
 import java.io.Serializable;
 
 
 /**
- * <p>A convenience class to represent name-value pairs.</p>
- * @since JavaFX 2.0
+ *
+ * @param <K> key.
+ * @param <V> value.
  */
-public class Pair<K,V> implements Serializable {
+public class Pair<K, V> implements Serializable {
 
     /**
      * Key of this <code>Pair</code>.
      */
     private K key;
-
-    /**
-     * Gets the key for this pair.
-     * @return key for this pair
-     */
-    public K getKey() { return key; }
-
     /**
      * Value of this this <code>Pair</code>.
      */
     private V value;
 
     /**
-     * Gets the value for this pair.
-     * @return value for this pair
-     */
-    public V getValue() { return value; }
-
-    /**
-     * Creates a new pair
-     * @param key The key for this pair
+     * Creates a new pair.
+     *
+     * @param key   The key for this pair
      * @param value The value to use for this pair
      */
-    public Pair( K key, V value) {
+    public Pair(final K key, final V value) {
         this.key = key;
         this.value = value;
     }
 
     /**
+     * Gets the key for this pair.
+     *
+     * @return key for this pair
+     */
+    public K getKey() {
+        return key;
+    }
+
+    /**
+     * Gets the value for this pair.
+     *
+     * @return value for this pair
+     */
+    public V getValue() {
+        return value;
+    }
+
+    /**
      * <p><code>String</code> representation of this
      * <code>Pair</code>.</p>
-     *
+     * <p>
      * <p>The default name/value delimiter '=' is always used.</p>
      *
-     *  @return <code>String</code> representation of this <code>Pair</code>
+     * @return <code>String</code> representation of this <code>Pair</code>
      */
     @Override
     public String toString() {
@@ -84,7 +64,7 @@ public class Pair<K,V> implements Serializable {
 
     /**
      * <p>Generate a hash code for this <code>Pair</code>.</p>
-     *
+     * <p>
      * <p>The hash code is calculated using both the name and
      * the value of the <code>Pair</code>.</p>
      *
@@ -97,32 +77,38 @@ public class Pair<K,V> implements Serializable {
         // these two parameters:
         //  name: a  value: aa
         //  name: aa value: a
-        return key.hashCode() * 13 + (value == null ? 0 : value.hashCode());
+        return key.hashCode()  + (value == null ? 0 : value.hashCode());
     }
 
     /**
      * <p>Test this <code>Pair</code> for equality with another
      * <code>Object</code>.</p>
-     *
+     * <p>
      * <p>If the <code>Object</code> to be tested is not a
      * <code>Pair</code> or is <code>null</code>, then this method
      * returns <code>false</code>.</p>
-     *
+     * <p>
      * <p>Two <code>Pair</code>s are considered equal if and only if
      * both the names and values are equal.</p>
      *
      * @param o the <code>Object</code> to test for
-     * equality with this <code>Pair</code>
+     *          equality with this <code>Pair</code>
      * @return <code>true</code> if the given <code>Object</code> is
      * equal to this <code>Pair</code> else <code>false</code>
      */
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
         if (o instanceof Pair) {
             Pair pair = (Pair) o;
-            if (key != null ? !key.equals(pair.key) : pair.key != null) return false;
-            if (value != null ? !value.equals(pair.value) : pair.value != null) return false;
+            if (key != null ? !key.equals(pair.key) : pair.key != null) {
+                return false;
+            }
+            if (value != null ? !value.equals(pair.value) : pair.value != null) {
+                return false;
+            }
             return true;
         }
         return false;
