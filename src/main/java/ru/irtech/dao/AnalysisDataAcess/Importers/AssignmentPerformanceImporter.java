de6e780662faf1c6b13a13a09cf7ba060b271770
Dilyara Galieva <galieva.d94@gmail.com>
2017-06-10 15:27:59 +0300

    importer that laods data for assignments

---
+++ src/main/java/ru/irtech/dao/AnalysisDataAcess/Importers/AssignmentPerformanceImporter.java
@@ -0,0 +1,72 @@
+package ru.irtech.dao.AnalysisDataAcess.Importers;
+
+import ru.irtech.form.PerformanceForm;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Galieva Dilyara 01.06.20117.
+ * Class dedicated to data selection from database
+ * selects information about grades
+ * by different grouppings
+ */
+public class AssignmentPerformanceImporter extends PerformanceClass {
+    /**
+     * method that queries assignment, quarter or year results.
+     *
+     * @param connection     connection to database
+     * @param pForm          contains all input data returned from view
+     *                       In case if groupings :
+     *                       1. student - all fields should be provided
+     *                       2. class - studentId should be null
+     *                       3. parallel - additionally to previous remove className
+     *                       4. school - additionally to previous remove grade
+     *                       5. all subjects - remove subjectName
+     * @return list of query results
+     */
+    List<String> queryResults(final Connection connection, final PerformanceForm pForm) {
+        List<String> result = new ArrayList<>();
+        try {
+            Statement st = connection.createStatement();
+            String queryString = "";
+            queryString = formAssignmentQuery();
+            String[] options = createOptions(pForm);
+            queryString = "select avg(R.RESULT), " + options[2] + queryString + options[0] + options[1];
+            System.out.println(queryString);
+            ResultSet rs = st.executeQuery(queryString);
+            while (rs.next()) {
+                result.add(rs.toString());
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * method that queries assignment results.
+     *
+     * @return query body before where clause
+     */
+    private String formAssignmentQuery() {
+        return " from SCHOOLS S\n"
+                + "join SCHOOLYEARS YSY on YSY.SCHOOLID=S.SCHOOLID\n"
+                + "join GLOBALYEARS Y on Y.GLOBALYEARID=YSY.GLOBALYEARID\n"
+                + "join CLASSES CL on CL.SCHOOLYEARID=YSY.SCHOOLYEARID\n"
+                + "join CLASSSUBJECTGROUPS CSG on CSG.PCLASSID=CL.CLASSID\n"
+                + "join SUBJECTGROUPS SG on SG.ID=CSG.ID\n"
+                + "join SUBJECTS SJ on SJ.SUBJECTID=SG.SUBJECTID\n"
+                + "join GLOBALSUBJECTS GSJ on GSJ.GLOBALSUBJID=SJ.GLOBALSUBJID\n"
+                + "join STUDENTSCLASSES SC on SC.CLASSID=CL.CLASSID\n"
+                + "join STUDENTS ST on ST.STUDENTID=SC.STUDENTID\n"
+                + "join USERS U on U.USERID=ST.STUDENTID\n"
+                + "join RESULTS R on ST.STUDENTID=R.STUDENTID\n"
+                + "join ASSIGNMENTS A on R.ASSIGNMENTID=A.ASSIGNMENTID and A.SGID = SG.ID\n";
+    }
+}
