638dc84df9763f15abdcb2a072ca86823b77f7b6
Liana Gareeva <liga.94@bk.ru>
2017-06-20 17:22:02 +0300

    new message about achievements added

--- src/main/java/ru/irtech/schedule/AdviseMessage.java
+++ src/main/java/ru/irtech/schedule/AdviseMessage.java
@@ -30,18 +30,19 @@ public class AdviseMessage {
             add("{\"id\":1004,\"type\":3,\"content\":\"<div>Учи и учись</div></a>\",\"link\":\"https://ad.admitad.com/g/ejv76qyygv095ebd86e5731073a875/?ulp=https%3A%2F%2Fupstudy.ru%2F\"}");
             add("{\"id\":1005,\"type\":3,\"content\":\"<div>Карта для умного шопинга!</div></a>\",\"link\":\"https://ad.admitad.com/g/6fz1m1rqml095ebd86e5806ff16a4a/\"}");
             add("{\"id\":1006,\"type\":4,\"content\":\"<div>А вы знали что песня Стюардесса по имени Жанна была написана в 1992 году? Вот ведь дела...</div>\",\"link\":\"\"}");
-            add("{\"id\":1006,\"type\":4,\"content\":\"<div>А ты знал, что в среднем дети смеются около 400 раз в день,а взрослые около 15 раз?</div>\",\"link\":\"\"}");
-            add("{\"id\":1006,\"type\":2,\"content\":\"<div>Вам выставили оценки по математике. Давай проверим что у тебя?</div>\",\"link\":\"http://innsgo.ru/\"}");
-            add("{\"id\":1006,\"type\":2,\"content\":\"<div>Сегодня 2 августа, до начала нового учебноого года осталось ровно 29 дней. Посмотри расписание летних мероприятий. </div>\",\"link\":\"http://innsgo.ru/\"}");
-            add("{\"id\":1006,\"type\":1,\"content\":\"<div>Сегодня в вашем городе холодно. Не забудь надеть шапку!</div>\",\"link\":\"\"}");
-            add("{\"id\":1006,\"type\":1,\"content\":\"<div>Cегодня 1 июня - день защиты детей!</div>\",\"link\":\"\"}");
+            add("{\"id\":1007,\"type\":4,\"content\":\"<div>А ты знал, что в среднем дети смеются около 400 раз в день,а взрослые около 15 раз?</div>\",\"link\":\"\"}");
+            add("{\"id\":1008,\"type\":2,\"content\":\"<div>Вам выставили оценки по математике. Давай проверим что у тебя?</div>\",\"link\":\"http://innsgo.ru/\"}");
+            add("{\"id\":1009,\"type\":2,\"content\":\"<div>Сегодня 2 августа, до начала нового учебноого года осталось ровно 29 дней. Посмотри расписание летних мероприятий. </div>\",\"link\":\"http://innsgo.ru/\"}");
+            add("{\"id\":1010,\"type\":1,\"content\":\"<div>Сегодня в вашем городе холодно. Не забудь надеть шапку!</div>\",\"link\":\"\"}");
+            add("{\"id\":1011,\"type\":1,\"content\":\"<div>Cегодня 1 июня - день защиты детей!</div>\",\"link\":\"\"}");
+            add("{\"id\":1012,\"type\":2,\"content\":\"<div>Ура, разблокировано достижение “5 пятерок подряд!”, ты один из 4% получивших его в этом учебном году</div>\",\"link\":\"http://innsgo.ru/\"}");
         }
     };
 
     /**
      *
      */
-    private static final long TIME_FOR_SENDING = 20000;
+    private static final long TIME_FOR_SENDING = 2000;
     /**
      * For WeSocket usage.
      */
@@ -74,5 +75,4 @@ public class AdviseMessage {
         int index = new Random().nextInt(advertisements.size());
         getTemplate().convertAndSend("/partnerAdvertisement", advertisements.get(index));
     }
-
 }
