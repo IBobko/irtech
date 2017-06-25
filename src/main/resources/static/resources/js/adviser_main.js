06ad9badf2893ac1c4bf31699bf3330c3dcaff0f
Dilyara Galieva <galieva.d94@gmail.com>
2017-06-26 00:13:14 +0300

    bugs regarding advisor skin choice fixed

--- src/main/resources/static/resources/js/adviser_main.js
+++ src/main/resources/static/resources/js/adviser_main.js
@@ -9,17 +9,18 @@ var InnopolisAdviser = {
         adviceLink: "",
         upClass: "toggle-up",
         downClass: "toggle-down",
-        changeOpen: "false",
-        DELAY: 700,
+        changeOpen: false,
+        DELAY: 300,
         clicks: 0,
         timer: null,
+        skins: ['minion', 'panda', 'raccoon', 'kitty'],
+        dblClick: false,
 
 // Method that checks if hardware running advisor is mobile or not
 // returns true if it is mobile, other way false
         checkIfMobile: function () {
             return !!(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
             || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)));
-
         }
         ,
 
@@ -61,7 +62,7 @@ var InnopolisAdviser = {
             //initial advice check
             this.checkInitAdvice();
 
-            $('.ui-loader').remove();
+            //$('.ui-loader').remove();
         }
         ,
 
@@ -85,17 +86,16 @@ var InnopolisAdviser = {
             this.hideAdvisor();
             this.separateClicks(this);
             this.onTapHold(this);
-            this.advisorSkinsLoad(this);
             this.clickOutside("advisorChange", this);
         }
         ,
 
         // loads skins for list of advisors for change
         advisorSkinsLoad: function (self) {
-            self.setImage("minion", "minion/advisor");
-            self.setImage("kitty", "kitty/advisor");
-            self.setImage("panda", "panda/advisor");
-            self.setImage("raccoon", "raccoon/advisor");
+            for (var i = 0; i < self.skins.length; i++) {
+                if (self.skins[i] != this.selectedAdvisor)
+                    self.setImage(self.skins[i], self.skins[i] + "/advisor");
+            }
         }
         ,
 
@@ -119,11 +119,14 @@ var InnopolisAdviser = {
                 // check that your clicked
                 // element has no id=info
                 // and is not child of info
+                console.log(e.target.id);
                 if (e.target.id != elementId && !$('#info').find(e.target).length) {
-                    if (e.target.id != "advisorImage") {
-                        if (self.changeOpen)
-                            $("#" + elementId).hide();
+                    //if (!self.dblClick) {
+                    if (self.changeOpen) {
+                        $("#" + elementId).hide();
+                        self.changeOpen = false;
                     }
+                    //}
                 }
             });
         }
@@ -163,7 +166,6 @@ var InnopolisAdviser = {
             }
             else if (self.adviceShowing) {
                 if (self.adviceType == 2 || self.adviceType == 3) {
-                    console.log(self.adviceLink);
                     var win = window.open(self.adviceLink, '_blank');
                     if (win) {
                         //Browser has allowed it to be opened
@@ -180,20 +182,16 @@ var InnopolisAdviser = {
         }
         ,
 
-// When the user double clicks on advisor
-// then ability to show other advisors is on
-        advisorDoubleClick: function (self) {
-            $('.ui-loader').remove();
+        showAdvisorChoice: function (self) {
+            // $('.ui-loader').remove();
             if (!self.changeOpen) {
                 self.showDiv("advisorChange");
                 self.changeOpen = true;
                 $(".skinContainer").click(function (e) {
-                    console.log(e.target.id);
                     self.selectSkin(e.target.id);
                 });
             }
             else {
-                self.hideDiv("advisorChange");
                 self.changeOpen = false;
             }
         }
@@ -202,18 +200,26 @@ var InnopolisAdviser = {
 // function which separates
 // double click from single click
         separateClicks: function (self) {
-            $("#advisorContent").click(function () {
+            $("#advisorContent").click(function (e) {
                 self.clicks++;  //count clicks
                 if (self.clicks === 1) {
                     self.timer = setTimeout(function () {
+                        self.dblClick = false;
                         self.advisorClick(self);
+                        $("#advisorChange").hide();
+                        self.changeOpen = false;
+                        clearTimeout(self.timer);
                         self.clicks = 0;             //after action performed, reset counter
                     }, self.DELAY);
                 } else {
+                    self.dblClick = true;
                     clearTimeout(self.timer);    //prevent single-click action
-                    self.advisorDoubleClick(self);
+                    clearTimeout(self.timer - self.clicks);    //prevent single-click action
+                    self.advisorSkinsLoad(self);
+                    self.showAdvisorChoice(self);
                     self.clicks = 0;             //after action performed, reset counter
                 }
+                e.stopPropagation();
             });
         }
         ,
@@ -221,8 +227,7 @@ var InnopolisAdviser = {
         onTapHold: function (self) {
             $("#advisorContent").bind("taphold", tapholdHandler);
             function tapholdHandler(event) {
-                console.log(3);
-                self.advisorDoubleClick(self);
+                self.showAdvisorChoice(self);
             }
         }
         ,
@@ -337,7 +342,6 @@ var InnopolisAdviser = {
 //loads advice from advice pool to view
 //  index   - advice index to load
         loadAdvice: function (advice) {
-            //console.log("LOAD ADVICE");
             this.adviceType = advice.type;
             this.adviceLink = advice.link;
             var adviceContent = advice.content;
