602d003ecf626d564ecb3068a39e5390ff6d4ec3
Igor Bobko <limit-speed@yandex.ru>
2017-08-05 00:02:02 +0300

    1a

--- src/main/java/ru/irtech/controller/LatencyController.java
+++ src/main/java/ru/irtech/controller/LatencyController.java
@@ -22,13 +22,13 @@ public class LatencyController {
     @ResponseBody
     public String row_data() {
         String ss = "Question,1,2,3,4,5,N\n" +
-                "Question 1,24,294,594,1927,376,3215\n" +
-                "Question 2,2,2,0,7,0,11\n" +
-                "Question 3,2,0,2,4,2,10\n" +
-                "Question 4,0,2,1,7,6,16\n" +
-                "Question 5,0,1,3,16,4,24\n" +
-                "Question 6,1,1,2,9,3,16\n" +
-                "Question 7,0,0,1,4,0,5\n" +
+                "Question 1,0,0,0,0,2,2\n" +
+                "Question 2,0,0,0,0,2,2\n" +
+                "Question 3,0,0,0,0,2,2\n" +
+                "Question 4,0,0,0,0,2,2\n" +
+                "Question 5,0,0,0,0,2,2\n" +
+                "Question 6,0,0,0,0,2,2\n" +
+                "Question 7,0,0,0,0,2,2\n" +
                 "Question 8,0,0,0,0,2,2";
         return ss;
     }
