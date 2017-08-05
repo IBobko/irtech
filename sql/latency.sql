efd4933cd84dc2e1a6aad003b03f733e890be6c1
Igor Bobko <limit-speed@yandex.ru>
2017-08-05 11:40:38 +0300

    1a

--- sql/latency.sql
+++ sql/latency.sql
@@ -14,4 +14,3 @@ INSERT  into irtech_ass_latency
     inner join CLASSMEETINGS cl on cl.CLASSMEETINGID=ass.CLASSMEETINGID
   where cl.DAY>'2012-09-01 00:00:00.000' and cl.DAY<'2013-01-08 00:00:00.000' AND
         ass.atypeid = 3;
-
